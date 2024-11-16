69dca960d31a3f795fa9eb6327c85c80d6a5298a
Ivan Tomov <ivanb.tomov1@gmail.com>
2024-11-16 11:27:34 +0200

    Initial commit

---
+++ CustomerRepository.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomerFinderApp
+{
+    public class CustomerRepository
+    {
+        private List<Customer> customers;
+
+        public CustomerRepository()
+        {
+            customers = new List<Customer>
+        {
+            new Customer { Id = 1, Name = "Ivan Tomov", Address = "ulitsa 123", NumberOfOrders = 5 },
+            new Customer { Id = 2, Name = "Yanko Yankov", Address = "ulitsa 456 ", NumberOfOrders = 3 },
+            new Customer { Id = 3, Name = "Tseko Krumov", Address = "ulitsa 789", NumberOfOrders = 7 },
+        };
+        }
+
+        public async Task<Customer?> FindCustomerByIdAsync(int id)
+        {
+            Random random = new Random();
+            int delay = random.Next(100, 1001);
+            await Task.Delay(delay);
+
+            return customers.FirstOrDefault(c => c.Id == id);
+        }
+
+        public async Task<Customer?[]> FindCustomersByIdsAsync(IEnumerable<int> ids)
+        {
+            var tasks = ids.Select(id => FindCustomerByIdAsync(id));
+            return await Task.WhenAll(tasks);
+        }
+
+    }
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Linq;
+using System.Windows;
+
+namespace CustomerFinderApp
+{
+    public partial class MainWindow : Window
+    {
+        private CustomerRepository repository;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            repository = new CustomerRepository();
+        }
+
+        private async void SearchButton_Click(object sender, RoutedEventArgs e)
+        {
+            ResultsListBox.Items.Clear();
+
+            try
+            {
+                
+                var idStrings = IdsTextBox.Text.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+                var ids = idStrings.Select(int.Parse);
+
+                
+                var customers = await repository.FindCustomersByIdsAsync(ids);
+
+                
+                foreach (var customer in customers)
+                {
+                    if (customer != null)
+                    {
+                        ResultsListBox.Items.Add($"{customer.Id}: {customer.Name}, {customer.Address} ({customer.NumberOfOrders} orders)");
+                    }
+                    else
+                    {
+                        ResultsListBox.Items.Add("Customer not found");
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Грешка: {ex.Message}", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+
+        }
+        private void IdsTextBox_TextChanged(object sender, System.Windows.Controls.TextChangedEventArgs e)
+        {
+            PlaceholderTextBlock.Visibility = string.IsNullOrEmpty(IdsTextBox.Text)
+                ? Visibility.Visible
+                : Visibility.Collapsed;
+        }
+
+    }
+}
